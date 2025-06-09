4a3d51bd2948d7e9ebb30864b5dee27320e1be4d
YongHee <doctorsean0425@gmail.com>
2025-06-09 21:31:25 +0900

    [Gold V] Title: Z, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,28 @@
+import sys
+
+n,r,c=map(int,sys.stdin.readline().split())
+
+answer=0
+while n!=0:
+
+    n-=1
+    size=2**n
+
+    if r<size and c<size:
+        pass
+
+    elif r<size and c>=size:
+        answer+=size*size 
+        c-=size
+        
+    elif r>=size and c<size:
+        answer+=size*size*2 
+        r-=size
+
+    else:
+        answer+=size*size*3
+        r-=size
+        c-=size
+    
+
+print(answer)
