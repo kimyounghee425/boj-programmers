ed2e61f39424a9601c322872f8f87d16c6558238
YongHee <doctorsean0425@gmail.com>
2025-11-25 12:00:31 +0900

    [Silver V] Title: 단어 정렬, Time: 916 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -18,7 +18,7 @@ const solution = (arr) => {
     const answer = [];
 
     for (let i = 0; i < arr.length; i++) {
-        if (i === 0 || arr[i] !== arr[i - 1]) {
+        if (arr[i] !== arr[i + 1]) {
             answer.push(arr[i]);
         }
     }
