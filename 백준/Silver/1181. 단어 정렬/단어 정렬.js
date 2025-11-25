3a90eb023f240038391e185346395b7161c61d78
YongHee <doctorsean0425@gmail.com>
2025-11-25 12:00:08 +0900

    [Silver V] Title: 단어 정렬, Time: 928 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+const sortFn = (a, b) => {
+    if (a.length > b.length) return 1;
+    else if (a.length === b.length) {
+        if (a === b) arr.pop(a);
+    }
+};
+
+const solution = (arr) => {
+    arr.sort((a, b) => {
+        if (a.length !== b.length) return a.length - b.length;
+        return a.localeCompare(b);
+    });
+
+    const answer = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            answer.push(arr[i]);
+        }
+    }
+
+    for (let i = 0; i < answer.length; i++) {
+        console.log(answer[i]);
+    }
+};
+
+const main = () => {
+    let N = Number(input[0]);
+    let arr = [];
+    for (let i = 1; i < N + 1; i++) {
+        arr.push(input[i]);
+    }
+    solution(arr);
+};
+
+main();
